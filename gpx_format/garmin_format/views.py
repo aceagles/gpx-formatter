5fa764a57a01a1d7c987776d3ecbaa9b3d6bb550
Adam Eagles <ad.c.eagles@gmail.com>
2022-10-15 18:53:56 +0100

    Download the new file automatically and provide a link

--- gpx_format/garmin_format/views.py
+++ gpx_format/garmin_format/views.py
@@ -5,11 +5,11 @@ from .models import GPXFile
 
 # Create your views here.
 def file_upload_view(request):
+    file = None
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         new_entry = GPXFile()
         new_entry.newfile.name = remove_waypoints(myfile)
         new_entry.save()
-        xml_gpx = myfile.read()
-        return HttpResponse("<textarea>"+str(xml_gpx)+"</textarea>")
-    return render(request, "upload/upload_form.html", {})
+        file = new_entry
+    return render(request, "upload/upload_form.html", {'file': file})
