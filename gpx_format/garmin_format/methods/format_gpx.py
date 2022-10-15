eb9ac5d435cfdab22d5f8e234cc49f64c576ade9
Adam Eagles <ad.c.eagles@gmail.com>
2022-10-15 19:48:26 +0100

    t

--- gpx_format/garmin_format/methods/format_gpx.py
+++ gpx_format/garmin_format/methods/format_gpx.py
@@ -4,11 +4,9 @@ import datetime
 
 
 def remove_waypoints(file):
+
+    # Parse the file and remove all the waypoints
     gpx = gpxpy.parse(file)
     gpx.waypoints = []
-    now = datetime.datetime.now()
-    now_str = f"{now.strftime('%d-%m-%yT%H-%M-%S-')}{file.name}"
-    file_path = f"gpx/{now_str}"
-    with open(f"media/{file_path}", "w+") as tmp:
-        tmp.write(gpx.to_xml())
-    return file_path
+    
+    return gpx.to_xml()
--- gpx_format/garmin_format/views.py
+++ gpx_format/garmin_format/views.py
@@ -2,14 +2,33 @@ from django.shortcuts import render
 from .methods.format_gpx import remove_waypoints
 from django.http import HttpResponse
 from .models import GPXFile
+import datetime
 
-# Create your views here.
 def file_upload_view(request):
     file = None
+    
+    # View self references so if a file is present then process and output
     if request.method == 'POST' and request.FILES['myfile']:
+
         myfile = request.FILES['myfile']
+
+        # Create a new entry in the GPX table
         new_entry = GPXFile()
-        new_entry.newfile.name = remove_waypoints(myfile)
+
+        #format the gpx
+        new_gpx = remove_waypoints(myfile)
+
+        # Create a file with the new gpx
+        now = datetime.datetime.now()
+        now_str = f"{now.strftime('%d-%m-%yT%H-%M-%S-')}{myfile.name}"
+        file_path = f"gpx/{now_str}"
+        with open(f"media/{file_path}", "w+") as tmp:
+            tmp.write(new_gpx)
+        
+        # Add the new file to the table
+        new_entry.newfile.name = file_path
         new_entry.save()
+
+        # Set the file variable to be returned to the template
         file = new_entry
     return render(request, "upload/upload_form.html", {'file': file})
